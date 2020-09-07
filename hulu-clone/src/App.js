ecef6ea19bf06049df8cee4f50f65eade5c046e8
Mukul Choudhury <mukulc998@gmail.com>
2020-09-07 19:31:53 +0530

    created

---
+++ hulu-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ hulu-clone/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+//API KEY  928b6836a751213685e929eae478407f
