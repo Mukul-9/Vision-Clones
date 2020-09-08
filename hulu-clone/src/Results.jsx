514648acf25f902a27e0a6edbfb679dd3c585678
Mukul Choudhury <mukulc998@gmail.com>
2020-09-08 08:44:31 +0530

    deploying...

--- hulu-clone/src/Results.jsx
+++ hulu-clone/src/Results.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Results.css";
 import VideoCard from "./VideoCard";
-function Results() {
+import axios from "./axios";
+import FlipMove from "react-flip-move";
+
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(selectedOption);
+      setMovies(request.data.results);
+      console.log(request);
+      return request;
+    }
+
+    fetchData();
+  }, [selectedOption]);
   return (
     <div className="results">
-      <VideoCard />
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- hulu-clone/src/VideoCard.jsx
+++ hulu-clone/src/VideoCard.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./VideoCard.css";
-function VideoCard() {
-  return <div className="videoCard"></div>;
-}
+import TextTruncate from "react-text-truncate";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+const VideoCard = forwardRef(({ movie }, ref) => {
+  return (
+    <div ref={ref} className="videoCard">
+      <img
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt=""
+      />
+      <TextTruncate
+        line={2}
+        element="p"
+        truncateText="..."
+        text={movie.overview}
+      />
+      <h2>{movie.original_title}</h2>
+      <h4>Rating: {movie.vote_average}</h4>
+    </div>
+  );
+});
 
 export default VideoCard;
